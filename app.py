e97b2e1d9cdfbf7845046da220052515cbc96876
Emmanuel Orozco <indi3.rok@gmail.com>
2023-09-07 13:53:55 +0200

    Added project with README and test

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import sqlite3
+
+app = Flask(__name__)
+
+DATABASE = 'guestbook.db'
+
+def get_db():
+    conn = sqlite3.connect(DATABASE)
+    return conn
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # If we send POST request to create user
+    # add logic
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+    if request.method == 'GET':
+    # If we send GET request to get all users
+    # add logic
+
+    return render_template('index.html')
